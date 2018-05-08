03fae53bfb6b730415c536400bd1ac8f77d4c2dd
Matthew Hillier <Matthew.P.Hillier@student.uts.edu.au>
2018-05-08 11:01:45 +1000

    Updated formatting

--- add.php
+++ add.php
@@ -1,21 +1,21 @@
-<?php
+ <?php
 session_start();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>add.php</title>
+    <title>add.php</title>
 </head>
 <body>
 
 <?php
 
 // connect to the products database
-$connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
+$connection = mysqli_connect( 'rerun', 'potiro', 'pcXZb(kL', 'poti' );
 
 // connection check statement
-if (!$connection) {
-	die("Can't connect to database!");
+if ( !$connection ) {
+    die( "Can't connect to database!" );
 }
 
 // get variables passed from image map
@@ -31,18 +31,18 @@ $_SESSION['product_id']=$product_id;
 // query string for database, replace '*' with variable from image map
 $query_string = "select * from products"; // used to test database
 //$query_string = "select $product_id from products"; 
- 
-// query the database and store the result
-$result=mysqli_query($connection,$query_string);
- 
 
-$num_rows=mysqli_num_rows($result);
+// query the database and store the result
+$result = mysqli_query( $connection, $query_string );
+
+
+$num_rows = mysqli_num_rows( $result );
 
 //using associative array
 // mysql_fetch_assoc: This function will return a row as an associative array where the column names will be the keys storing corresponding value.
 
 // building the table to display the results from the query, only if there are results to display
-if ($num_rows > 0 ) {
+if ( $num_rows > 0 ) {
     print "<table border='0'>";
     print "<tr>";
     print "<td>Product ID</td>";
@@ -52,32 +52,32 @@ if ($num_rows > 0 ) {
     print "<td>In Stock</td>";
     print "<td>Quantity</td>";
     print "</tr>";
-
+    
     // will loop through all data from results and display the required information
-     while ( $a_row = mysqli_fetch_assoc($result) ) {
-         print "<tr>\n";
-         print "<td>$a_row[product_id]</td>";
-         print "<td>$a_row[product_name]</td>";
-         print "<td>$a_row[unit_price]</td>";
-         print "<td>$a_row[unit_quantity]</td>";
-         print "<td>$a_row[in_stock]</td>";
-
-         // create the quantity text box, and submit button, ready to send to cart.php
-         print "<td>";
-         print "<form action="cart.php" method="post">";
-         print "<input type="text" name="quantity" size="3"><input type="submit" name="add" value="add">";
-     	 print "</form>";
-     	 print "</td>";
-     	 print "</tr>";
+    while ( $a_row = mysqli_fetch_assoc( $result ) ) {
+        print "<tr>\n";
+        print "<td>$a_row[product_id]</td>";
+        print "<td>$a_row[product_name]</td>";
+        print "<td>$a_row[unit_price]</td>";
+        print "<td>$a_row[unit_quantity]</td>";
+        print "<td>$a_row[in_stock]</td>";
+        
+        // create the quantity text box, and submit button, ready to send to cart.php
+        print "<td>";
+        print "<form action="cart.php" method="post">";
+        print "<input type="text" name="quantity" size="3"><input type="submit" name="add" value="add">";
+        print "</form>";
+        print "</td>";
+        print "</tr>";
     }
-	
+    
     print "</table>";
 }
 
 //close the connection to the database
-mysqli_close($connection);
+mysqli_close( $connection );
 
 ?>
 
 </body>
-</html>
+</html> 
--- cart.php
+++ cart.php
@@ -1,10 +1,10 @@
-<?php
+ <?php
 session_start();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Cart.php</title>
+    <title>Cart.php</title>
 
 </head>
 <body>
@@ -13,62 +13,62 @@ session_start();
 
 <table>
 <tr>
-	<td>Product Name</td>
-	<td>Unit Price</td>
-	<td>Unit Quantity</td>
-	<td>Required Quantity</td>
-	<td>Total Cost</td>
+    <td>Product Name</td>
+    <td>Unit Price</td>
+    <td>Unit Quantity</td>
+    <td>Required Quantity</td>
+    <td>Total Cost</td>
 </tr>
 
 
 <?php
 //retrieve product id from the session
-$product_id = $_SESSION['product_id'];
+$product_id = $_SESSION[ 'product_id' ];
 
 //retrieve quantity sent from past page
-$quantity=$_REQUEST['quantity'];
+$quantity = $_REQUEST[ 'quantity' ];
 
 //connect to database
-$connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
+$connection = mysqli_connect( 'rerun', 'potiro', 'pcXZb(kL', 'poti' );
 
 // connection check statement
-if (!$connection) {
-	die("Can't connect to database!");
+if ( !$connection ) {
+    die( "Can't connect to database!" );
 }
 
 //query the database for the data we need
 $query_string = "select $product_id from products";
-$result=mysqli_query($connection,$query_string);
+$result       = mysqli_query( $connection, $query_string );
 
-     while ( $a_row = mysqli_fetch_assoc($result) ) {
-         $cart ="<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>".$a_row[unit_price]*$quantity."</td></tr>";
+while ( $a_row = mysqli_fetch_assoc( $result ) ) {
+    $cart = "<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>" . $a_row[ unit_price ] * $quantity . "</td></tr>";
 }
 
 
 //store the shopping cart
-$cart= array();
+$cart = array( );
 
 //test if cart has contents already and print them
 // add check for quantity not empty(for delete button purpose)
-if (!empty($cart)) {
-	print $cart;
-	     if (!empty($result)) {     
-     while ( $a_row = mysqli_fetch_assoc($result) ) {
-         $cart ="<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>".$_SESSION['total_cost']=$_SESSION['total_cost']+($a_row[unit_price]*$quantity)."</td></tr>";
-}
-}
-	
+if ( !empty( $cart ) ) {
+    print $cart;
+    if ( !empty( $result ) ) {
+        while ( $a_row = mysqli_fetch_assoc( $result ) ) {
+            $cart = "<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>" . $_SESSION[ 'total_cost' ] = $_SESSION[ 'total_cost' ] + ( $a_row[ unit_price ] * $quantity ) . "</td></tr>";
+        }
+    }
+    
 }
 //first time adding items to cart
-else{
-     if (!empty($result)) {     
-     while ( $a_row = mysqli_fetch_assoc($result) ) {
-         $cart ="<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>".$_SESSION['total_cost']=$a_row[unit_price]*$quantity."</td></tr>";
+else {
+    if ( !empty( $result ) ) {
+        while ( $a_row = mysqli_fetch_assoc( $result ) ) {
+            $cart = "<tr>\n<td>$a_row[product_name]</td><td>$a_row[unit_price]</td><td>$a_row[unit_quantity]</td><td>$quantity</td><td>" . $_SESSION[ 'total_cost' ] = $a_row[ unit_price ] * $quantity . "</td></tr>";
+        }
+    }
+    print $cart;
 }
-}
-print $cart;
-}
-$numprod = count($cart)
+$numprod = count( $cart );
 print "<tr>\n<td>Number of Products</td><td>$numprod</td></tr>";
 print "<tr>\n<td>Shopping Cart Total</td><td>$_SESSION['total_cost']</td></tr>";
 ?>
@@ -76,4 +76,4 @@ print "<tr>\n<td>Shopping Cart Total</td><td>$_SESSION['total_cost']</td></tr>";
 </table>
 
 </body>
-</html>
+</html> 
