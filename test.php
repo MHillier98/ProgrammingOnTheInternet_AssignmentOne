1076d9aacda0db081348e62478d1fba175014de8
Robert_Corney <rtcorney.96@gmail.com>
2018-05-15 18:25:07 +1000

    added clear button(it works!)

--- test.php
+++ test.php
@@ -21,28 +21,39 @@ if ( !$connection ) {
 <h1>Shopping Cart</h1>
 
 <?
+
+if(isset($_REQUEST['clear'])){
+        //clear any data from cart array
+        unset($cart);
+        //clear all data from the session variables
+        session_unset($_SESSION['numprod']);
+        session_unset($_SESSION['total_cost']);
+        session_unset($_SESSION['product_id']);
+}
+else{
+
 // get Variables
 $product_id=$_SESSION['product_id'];
 $quantity=$_REQUEST['quantity'];
 $cart=array();
 echo $product_id;
 echo $quantity;
-?>
 
-<?
+
+
 //Construct query
 $query="select * from products where product_id=$product_id";
 echo $query;
-?>
 
-<?
+
+
 $result = mysqli_query( $connection, $query);
         if (empty($result)) {
             echo "empty result";
         }
-?>
 
 
+print'
     <table style="align:center;">
     <tr>
         <td>Product Name</td>
@@ -50,9 +61,9 @@ $result = mysqli_query( $connection, $query);
         <td>Unit Quantity</td>
         <td>Required Quantity</td>
         <td>Total Cost</td>
-    </tr>
+    </tr>';
 
-<?
+
 /*
 if ( !empty( $_SESSION['cart'] ) ) {
 //add product to cart array
@@ -80,9 +91,9 @@ else{
         $cart= "<tr>\n"."<td>$a_row[product_name]</td>"."<td>$a_row[unit_price]</td>"."<td>$a_row[unit_quantity]</td>"."<td>$quantity</td>"."<td>".$cost."</td></tr>";
         print $cart;
     }
-?>
 
-<?
+
+
 $numprod=$_SESSION['numprod'];
 $numprod=$numprod+1;
 print "<tr>\n<td>Number of Products</td><td>$numprod</td></tr>";
@@ -92,20 +103,24 @@ $total_cost=$_SESSION['total_cost'];
 $total_cost=$total_cost+$cost;
 print "<tr>\n<td>Shopping Cart Total</td><td>$total_cost</td></tr>";
 $_SESSION['total_cost']=$total_cost;
-?>
 
-</table>
 
-    <table style="align:center;">
+print '</table>';
+
+if (!empty($cart)) {
+    print'<table style="align:center;">
         <tr>
-            <td><form action="cart.php" method="post"><input type="submit" name="clear" value="clear"></form></td>
+            <td><form action="test.php" method="get"><input type="submit" name="clear" value="clear"></form></td>
             <td><form action="checkout.php" method="post"><input type="submit" name="checkout" value="checkout"></form></td>
         </tr>
-    </table>
+    </table>';
+}
+
+$_SESSION['cart']=$cart;
+}
+?>
 
 <?
-$_SESSION['cart']=$cart;
-
 msqli_close($connection);
 ?>
 </body>
